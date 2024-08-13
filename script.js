8aaca6e161b22f06ff7379e50f60d3239fa098e0
Jacob <100544538+jongseokleedev@users.noreply.github.com>
2024-08-13 16:09:50 +0900

    added commented lines

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelectorAll(".order-button").forEach((button) => {
     console.log(`${button.innerText} Event`);
     // console.log(e.target); //target Property
     // console.log(e.currentTarget); //currentTarget Property
-    e.stopPropagation();
+    // e.stopPropagation();
   });
 });
 
@@ -12,18 +12,18 @@ document.querySelectorAll(".order-button").forEach((button) => {
 document.querySelector("#table1").addEventListener("click", function (e) {
   console.log("테이블 1 Event");
   // console.log(e.target); //target Property
-  console.log(e.currentTarget); //currentTarget Property
+  // console.log(e.currentTarget); //currentTarget Property
 });
 
 document.querySelector("#table2").addEventListener("click", function (e) {
   console.log("테이블 2 Event");
   // console.log(e.target); //target Property
-  console.log(e.currentTarget); //currentTarget Property
+  // console.log(e.currentTarget); //currentTarget Property
 });
 
 // 식당 구역 클릭 이벤트 핸들러
 document.querySelector("#restaurant").addEventListener("click", function (e) {
   console.log("식당 Event");
   // console.log(e.target); //target Property
-  console.log(e.currentTarget); //currentTarget Property
+  // console.log(e.currentTarget); //currentTarget Property
 });
